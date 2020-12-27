2579a7ef5e01d126abbf536ab6a8d2ecd05f3a1e
vasukushwah <vkpatel422@gmail.com>
2020-12-27 10:30:03 +0530

    - delay added

--- checkConnection.py
+++ checkConnection.py
@@ -46,6 +46,8 @@ if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
+    logging.info("Waiting for RPZW....")
+    time.sleep(180)
     while True:
       checkwificonnection()
       time.sleep(5)
